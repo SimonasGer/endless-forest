462141ae8e5fedd0c017a8b5a1b37b99dc58593b
Simonas Gerulis <simonasgerulis1@gmail.com>
2025-08-05 20:12:33 +0300

    basic movement

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public InputAction inputAction;
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    private void OnEnable()
     {
-        
+        inputAction.Enable();
     }
 
-    // Update is called once per frame
+    private void OnDisable()
+    {
+        inputAction.Disable();
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
-        
+        moveInput = inputAction.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput.normalized * moveSpeed;
     }
 }
